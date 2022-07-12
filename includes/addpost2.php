622f6f2f88dc6e6c02a09c5d7b34057168c85784
Chinmaya Kumar Biswal <chinmayakumarbiswal16045@gmail.com>
2022-07-13 01:12:58 +0530

    Add files via upload

---
+++ includes/addpost2.php
@@ -0,0 +1,60 @@
+<?php
+$IAM_KEY = 'AKIAWBYXXUVGJAVLT5VF';
+$IAM_SECRET = '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';
+$bucket = 'arn:aws:s3:::sakhyat.tect';
+
+
+require 'vendor/autoload.php';
+use Aws\S3\S3Client;
+use Aws\S3\Exception\S3Exception;
+
+$s3 = new S3Client([
+    'version' => 'latest',
+    'region'  => 'ap-south-1',
+    'credentials' => [
+        'key' => $IAM_KEY,
+        'secret' => $IAM_SECRET
+    ]
+]);
+
+
+
+
+require('database.php');
+if(isset($_POST['addpost'])){
+    $ptitle=mysqli_real_escape_string($db,$_POST['post_title']);
+    $pcontent=mysqli_real_escape_string($db,$_POST['post_content']);
+    $pcategory=$_POST['post_category'];
+    $query="INSERT INTO posts (title,content,category_id) VALUES('$ptitle','$pcontent',$pcategory)";
+    $run=mysqli_query($db,$query);
+
+    $post_id=mysqli_insert_id($db);
+    $image_name=$_FILES['post_image']['name'];
+    $image_tmp=$_FILES['post_image']['tmp_name'];
+
+    echo $image_name;
+    echo $image_tmp;
+
+    try {
+        // Upload data.
+        $result = $s3->putObject([
+            'Bucket' => $bucket,
+            'Key'    => $image_name,
+            'SourceFile' => $image_tmp,
+            'ACL' => 'public-read'
+        ]);
+    
+        // Print the URL to the object.
+    } catch (S3Exception $e) {
+        echo $e->getMessage() . PHP_EOL;
+    }    
+    
+    if($result){
+        $query="INSERT INTO images (post_id,image) VALUES('$post_id','$image_name')";
+        $run=mysqli_query($db,$query);
+    }
+
+   // header('location:../adminlogin/index.php');
+}
+
+?>
